0ec3184d7872eef7d61baaa78b27523589a5e40d
MS7773 <sethmonil30@gmail.com>
2025-03-29 12:19:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID monil777/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID monil777/$JOB_NAME:latest
 
-docker push vaibhavjoshi26/$JOB_NAME:$BUILD_ID
+docker push monil777/$JOB_NAME:$BUILD_ID
 
-docker push vaibhavjoshi26/$JOB_NAME:latest
+docker push monil777/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:$BUILD_ID vaibhavjoshi26/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID monil777/$JOB_NAME:$BUILD_ID monil777/$JOB_NAME:latest
